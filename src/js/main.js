03960ddd29c057c74bf0672460e11fa191aa9fc1
Ana Cañete <ana.canete13@gmail.com>
2026-04-11 11:09:48 +0800

    Add productos.js and revamp UI/layout/styles

---
+++ src/js/main.js
@@ -0,0 +1,1 @@
+import { productos } from "./productos.js";
---
+++ src/js/productos.js
@@ -0,0 +1,217 @@
+const productos = [
+  // Figuras
+  {
+    id: "figura-01",
+    titulo: "Figura 01",
+    imagen: "./src/img/figuras/1.jpeg",
+    categoria: {
+      nombre: "Figuras",
+      id: "figuras",
+    },
+    precio: 19.99,
+  },
+  {
+    id: "figura-02",
+    titulo: "Figura 02",
+    imagen: "./src/img/figuras/2.jpeg",
+    categoria: {
+      nombre: "Figuras",
+      id: "figuras",
+    },
+    precio: 19.99,
+  },
+  {
+    id: "figura-03",
+    titulo: "Figura 03",
+    imagen: "./src/img/figuras/3.jpeg",
+    categoria: {
+      nombre: "Figuras",
+      id: "figuras",
+    },
+    precio: 19.99,
+  },
+  {
+    id: "figura-04",
+    titulo: "Figura 04",
+    imagen: "./src/img/figuras/4.jpeg",
+    categoria: {
+      nombre: "Figuras",
+      id: "figuras",
+    },
+    precio: 19.99,
+  },
+  {
+    id: "figura-05",
+    titulo: "Figura 05",
+    imagen: "./src/img/figuras/5.jpeg",
+    categoria: {
+      nombre: "Figuras",
+      id: "figuras",
+    },
+    precio: 19.99,
+  },
+  {
+    id: "figura-06",
+    titulo: "Figura 06",
+    imagen: "./src/img/figuras/6.jpeg",
+    categoria: {
+      nombre: "Figuras",
+      id: "figuras",
+    },
+    precio: 19.99,
+  },
+  {
+    id: "figura-07",
+    titulo: "Figura 07",
+    imagen: "./src/img/figuras/7.jpeg",
+    categoria: {
+      nombre: "Figuras",
+      id: "figuras",
+    },
+    precio: 19.99,
+  },
+  {
+    id: "figura-08",
+    titulo: "Figura 08",
+    imagen: "./src/img/figuras/8.jpeg",
+    categoria: {
+      nombre: "Figuras",
+      id: "figuras",
+    },
+    precio: 19.99,
+  },
+  {
+    id: "figura-09",
+    titulo: "Figura 09",
+    imagen: "./src/img/figuras/9.jpeg",
+    categoria: {
+      nombre: "Figuras",
+      id: "figuras",
+    },
+    precio: 19.99,
+  },
+  {
+    id: "figura-10",
+    titulo: "Figura 10",
+    imagen: "./src/img/figuras/10.jpeg",
+    categoria: {
+      nombre: "Figuras",
+      id: "figuras",
+    },
+    precio: 19.99,
+  },
+
+  //Accesorios
+  {
+    id: "accesorio-01",
+    titulo: "Accesorio 01",
+    imagen: "./src/img/accesorios/1.jpeg",
+    categoria: {
+      nombre: "Accesorios",
+      id: "accesorios",
+    },
+    precio: 19.99,
+  },
+  {
+    id: "accesorio-02",
+    titulo: "Accesorio 02",
+    imagen: "./src/img/accesorios/2.jpeg",
+    categoria: {
+      nombre: "Accesorios",
+      id: "accesorios",
+    },
+    precio: 19.99,
+  },
+  {
+    id: "accesorio-03",
+    titulo: "Accesorio 03",
+    imagen: "./src/img/accesorios/3.jpeg",
+    categoria: {
+      nombre: "Accesorios",
+      id: "accesorios",
+    },
+    precio: 19.99,
+  },
+  {
+    id: "accesorio-04",
+    titulo: "Accesorio 04",
+    imagen: "./src/img/accesorios/4.jpeg",
+    categoria: {
+      nombre: "Accesorios",
+      id: "accesorios",
+    },
+    precio: 19.99,
+  },
+  {
+    id: "accesorio-05",
+    titulo: "Accesorio 05",
+    imagen: "./src/img/accesorios/5.jpeg",
+    categoria: {
+      nombre: "Accesorios",
+      id: "accesorios",
+    },
+    precio: 19.99,
+  },
+
+  //Remeras
+  {
+    id: "remera-01",
+    titulo: "Remera 01",
+    imagen: "./src/img/remeras/1.jpeg",
+    categoria: {
+      nombre: "Remeras",
+      id: "remeras",
+    },
+    precio: 19.99,
+  },
+  {
+    id: "remera-02",
+    titulo: "Remera 02",
+    imagen: "./src/img/remeras/2.jpeg",
+    categoria: {
+      nombre: "Remeras",
+      id: "remeras",
+    },
+    precio: 19.99,
+  },
+  {
+    id: "remera-03",
+    titulo: "Remera 03",
+    imagen: "./src/img/remeras/3.jpeg",
+    categoria: {
+      nombre: "Remeras",
+      id: "remeras",
+    },
+    precio: 19.99,
+  },
+  {
+    id: "remera-04",
+    titulo: "Remera 04",
+    imagen: "./src/img/remeras/4.jpeg",
+    categoria: {
+      nombre: "Remeras",
+      id: "remeras",
+    },
+    precio: 19.99,
+  },
+  {
+    id: "remera-05",
+    titulo: "Remera 05",
+    imagen: "./src/img/remeras/5.jpeg",
+    categoria: {
+      nombre: "Remeras",
+      id: "remeras",
+    },
+    precio: 19.99,
+  },
+  {
+    id: "remera-06",
+    titulo: "Remera 06",
+    imagen: "./src/img/remeras/6.jpeg",
+    categoria: {
+      nombre: "Remeras",
+      id: "remeras",
+    },
+    precio: 19.99,
+  },
+];
